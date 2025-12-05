5efca93611bb4833cb42e4829aefd761fca7a646
Gaurav Yadav <gauravyatwork@gmail.com>
2025-12-05 17:23:20 +0530

    complete follow-up from code review

--- python-module/pythonMovieStreamingAnalytics/passwordValidator.py
+++ python-module/pythonMovieStreamingAnalytics/passwordValidator.py
@@ -3,8 +3,25 @@ class Validator:
     A utility class to validate a password and its confirmation string.
     '''
     def __init__(self, password, confirmation):
+        Validator.validate(password)
+        Validator.validate(confirmation)
         self.password = password
         self.confirmation = confirmation
+
+    @staticmethod
+    def validate(password):
+        '''
+        Checks if given password is alphanumeric
+        :param password: String
+        :return: boolean
+        '''
+        if len(password) < 8:
+            raise ValueError("Length of input is smaller than 8")
+        for c in password:
+            if c.isalnum() is False:
+                raise ValueError("Password is not alphanumeric")
+        return
+
 
     def get_password_length(self):
         return len(self.password)
@@ -30,4 +47,9 @@ def main():
     print('Strings match:', validator.is_equal())
 
 if __name__ == "__main__":
-    main()
+    try :
+        main()
+    except ValueError as value_error:
+        print("Value Error:", value_error)
+    except Exception as exception:
+        print("Error Occurred:", exception)
